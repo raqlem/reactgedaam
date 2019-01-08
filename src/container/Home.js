df3c3b33d9a0b1660cb7287d704bb7970fb77a97
Raquel Lemos <raqlem@gmail.com>
2019-01-08 07:57:30 -0200

    Add card component properties

--- src/container/Home.js
+++ src/container/Home.js
@@ -2,11 +2,32 @@ import React, { Component} from 'react';
 import HomeCard from '../ui/HomeCard';
 
 export default class Home extends Component {
+
+  homeCardSubscriptions = {
+    title: 'Inscrições',
+    text: 'Gerencia Inscrições',
+    action: () => alert('Página Inscrições acionada')
+  }
+
+  homeCardStudents = {
+    title: 'Alunos',
+    text: 'Gerencia Alunos',
+    action: () => alert('Página Alunos acionada')
+  }
+
+  homeCardClasses = {
+    title: 'Turmas',
+    text: 'Gerencia Turmas',
+    action: () => alert('Página Turmas acionada')
+  }
+
   render() {
     return (
       <div className="container text-center">
         <div className="row">
-          <HomeCard />
+          <HomeCard {...this.homeCardSubscriptions}/>
+          <HomeCard {...this.homeCardStudents}/>
+          <HomeCard {...this.homeCardClasses}/>
         </div>
       </div>
     );
--- src/ui/HomeCard.js
+++ src/ui/HomeCard.js
@@ -1,13 +1,13 @@
 import React from 'react';
 
-const HomeCard = () => (
-  <div className="col-12 col-sm-4">
+const HomeCard = (props) => (
+  <div className="col-12 col-sm-4 col-md-6">
     <div className="card" style={{width: '18rem'}}>
       <img src="https://www.bandab.com.br/wp-content/uploads/2018/01/medicina.jpg" style={{ paddingTop: '3px' }} className="card-img-top rounded mx-auto" alt="..." />
       <div className="card-body">
-        <h5 className="card-title">Card title</h5>
-        <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-        <a href="#" className="btn btn-primary">Go somewhere</a>
+        <h5 className="card-title">{props.title}</h5>
+        <p className="card-text">{props.text}</p>
+        <a href="#" className="btn btn-primary" onClick={props.action}>Go somewhere</a>
       </div>
     </div>
   </div>
